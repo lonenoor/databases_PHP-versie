1c63c34a7c2025cec3bd5039e687ed1475802c53
lonenoor <154393@csgaugustinus.nl>
2023-04-19 10:36:23 +0000

    gjg

---
+++ databases/jolanda.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS `jolanda`;
+
+CREATE DATABASE "jolanda"
+
+USE `jolanda`;
+
+CREATE TABLE `Haarproducten`( 
+`id` int AUTO_INCREMENT PRIMARY KEY
+`product` text NOT NULL
+`prijs` int NOT NULL
+); 
+
+INSERT INTO `haarproduchten` (`id`, `product`, `prijs`) VALUES
+(1, 'shampoo', `15`),
+(2, 'conditioner', `20`),
+(3, 'zilvershampoo', `12`); 
+
+-- test
