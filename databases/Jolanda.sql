df1ccd4cce018abd6fed827fab0f8799da6e435f
lonenoor <154393@csgaugustinus.nl>
2023-04-19 10:28:51 +0000

    ghghf

---
+++ databases/Jolanda.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS `Jolanda`;
+
+CREATE DATABASE "Jolanda"
+
+USE `Jolanda`;
+
+CREATE TABLE `Haarproducten`( 
+`id` int AUTO_INCREMENT PRIMARY KEY
+`product` text NOT NULL
+`prijs` int NOT NULL
+); 
+
--- databases/examentraining.sql
+++ databases/examentraining.sql
@@ -1,1 +1,14 @@
 DROP DATABASE IF EXISTS `examentraining`;
+
+CREATE DATABASE "examentraining"
+
+USE `examentraining`;
+CREATE TABLE `vakken` (
+`vak` varchar(16) PRIMARY KEY,
+`docentcode` varchar(3) NOT NULL,
+`docentnaam` text NOT NULL,
+`lokaal` tekst
+); 
+CREATE TABLE `leerlingen`(
+    `id_leerling` int AUTO_INCREMENT PRIMARY KEY, 
+)
