798eae46608ef3ab2957beeeec0bc52cdc69a43f
lonenoor <154393@csgaugustinus.nl>
2023-04-19 11:05:26 +0000

    fix

--- databases/jolanda.sql
+++ databases/jolanda.sql
@@ -5,14 +5,12 @@ CREATE DATABASE `jolanda`;
 USE `jolanda`;
 
 CREATE TABLE `haarproducten`( 
-`id` int AUTO_INCREMENT PRIMARY KEY,
-`product` text NOT NULL,
-`prijs` int NOT NULL
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `product` text NOT NULL,
+    `prijs` int NOT NULL
 ); 
 
 INSERT INTO `haarproduchten` (`id`, `product`, `prijs`) VALUES
 (1, 'shampoo', `15`),
 (2, 'conditioner', `20`),
 (3, 'zilvershampoo', `12`); 
-
--- test
